7e314f39f3066d056c89fbd4248eb17eef80bf63
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2024-01-03 09:26:44 +0545

    Fix data_index.txt not being saved, update model specs in separate thread and use relative paths.

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -1,25 +1,41 @@
 import os
 from pathlib import Path
 import sys
-from config import BATTERY_LOG_PATH, DATA_CHUNKS, POLLING_INTERVAL
 import time
 import numpy as np
 import pandas as pd
 from tensorflow import keras
 from keras.models import load_model
 import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping
-
+from keras.callbacks import EarlyStopping
+from threading import Thread
 
 from sklearn.preprocessing import MinMaxScaler
 
-data_index= 0
+from config import BATTERY_LOG_PATH, DATA_CHUNKS, POLLING_INTERVAL, MODEL_FOLDER_NAME, RESOURCES_FOLDER_NAME, OUTPUT_SCALER_NAME, DATA_INDEX_FILE_NAME
+from pathlib import Path
 
-if os.listdir("./").__contains__("Battery_model"):
-    Battery_model = load_model("Battery_model")
-    output_scaler= np.load("../Battery_model/Output_scaler.npy")
-    with open('../Battery_model/dataIndex.txt', 'r') as f:
+
+MODEL_TRAINING_FILE_PATH = Path(__file__)
+RESOURCESS_PATH = MODEL_TRAINING_FILE_PATH.parents[1].joinpath(RESOURCES_FOLDER_NAME)
+
+BATTERY_MODEL_PATH = RESOURCESS_PATH.joinpath(MODEL_FOLDER_NAME)
+MINMAX_SCALAR_PATH = RESOURCESS_PATH.joinpath(OUTPUT_SCALER_NAME)
+DATA_INDEX_FILE_PATH = RESOURCESS_PATH.joinpath(DATA_INDEX_FILE_NAME)
+
+if not os.path.exists(RESOURCESS_PATH):
+    print(f"The path {RESOURCESS_PATH} doesn't exits so creating it.")
+    os.mkdir(RESOURCESS_PATH)
+
+data_index = 0
+
+if os.path.exists(BATTERY_MODEL_PATH):
+    Battery_model = load_model(BATTERY_MODEL_PATH)
+    loaded_scaler = np.load(MINMAX_SCALAR_PATH, allow_pickle=True)
+    output_scaler= loaded_scaler.item()
+    with open(DATA_INDEX_FILE_PATH, 'r') as f:
         data_index = int(f.read())
+    model_previously_present = True
 else:
     Battery_model = tf.keras.Sequential()
     Battery_model.add(tf.keras.layers.BatchNormalization())
@@ -37,7 +53,7 @@ else:
     early_stopping = EarlyStopping(
         monitor='val_loss', patience=5, restore_best_weights=True)
 
-output_scaler = MinMaxScaler(feature_range=(-1, 1))
+    output_scaler = MinMaxScaler(feature_range=(-1, 1))
 
 
 def model_training():
@@ -50,26 +66,45 @@ def model_training():
     
     while True:
         # Importing data in chunks of 1000
-        file_chunks = pd.read_csv(BATTERY_LOG_PATH, skiprows=range(0,data_index), chunksize=DATA_CHUNKS)
-        while True:
+        chunksize = DATA_CHUNKS
+        data_shape_1 = log_data.shape[0]
+        if data_shape_1 < DATA_CHUNKS and data_index == 0:
+            chunksize = data_shape_1
+
+        if data_index != 0 and (data_shape_1 - data_index) < DATA_CHUNKS: 
+            time.sleep(POLLING_INTERVAL)
+            continue
+
+        file_chunks = pd.read_csv(BATTERY_LOG_PATH, skiprows=range(0,data_index), chunksize=chunksize)
+        # 
+        for chunk in file_chunks:
             try:
-               model_fitting(datas=next(file_chunks))
-            except:
+                model_fitting(datas=chunk)
+            except Exception as e:
                 time.sleep(POLLING_INTERVAL)
 
+from model_prediction import update_model_and_scaler
 
 def model_fitting(datas):
+    global data_index, output_scaler
+
     X_log = datas[['Voltage[V]', 'Energy[J]','Energy_Rate[J/s]', 'Time Difference[s]', 'Battery State']]
     y_log = datas['Capacity Difference[J]']
     capacity_difference_column = y_log.values.reshape(-1, 1)
-    Y_normalized = output_scaler.fit_transform(
-        capacity_difference_column)
-    output_scaler.inverse_transform(Y_normalized)
+
+    Y_normalized = output_scaler.fit_transform(capacity_difference_column)
+    
+    # output_scaler.inverse_transform(Y_normalized)
+
     Battery_model.fit(X_log, Y_normalized, epochs=50, batch_size=5,
                         validation_split=0.2, callbacks=[early_stopping])
-    Battery_model.save("Battery_model")
-    np.save("../Battery_model/Output_scaler.npy",output_scaler)
-    data_index+=datas.shape[1]
-    with open('../Battery_model/dataIndex.txt', 'w') as f:
-        f.write(data_index+DATA_CHUNKS)
-                
+    # do this on separate thread
+    p = Thread(target=update_model_and_scaler, args=[Battery_model, output_scaler], daemon=True)
+    p.start()
+
+    Battery_model.save(BATTERY_MODEL_PATH)
+    np.save(MINMAX_SCALAR_PATH,output_scaler, allow_pickle=True)
+    data_index += datas.shape[0]
+
+    with open(DATA_INDEX_FILE_PATH, 'w') as f:
+        f.write(str(data_index))
